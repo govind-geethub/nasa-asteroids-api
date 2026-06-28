5dcd32b909484b079e82a13a645a367ed847ebbc
govind-geethub <avin14a@gmail.com>
2026-06-28 23:34:10 +0530

    performed all the CRUD operations

--- main.go
+++ main.go
@@ -14,4 +14,21 @@ func main() {
 
 	InitStorage(asteroids)
 	fmt.Printf("Fetched %d asteroids from NASA. \n", len(asteroids))
+
+	var savedAsteroids = GetAsteroids()
+	fmt.Printf("number of saved asteroids are : %d \n", len(savedAsteroids))
+
+	// create test
+	var customAsteroid Asteroid
+	CreateAsteroid((customAsteroid))
+
+	var newAsteroids = GetAsteroids()
+	fmt.Printf("after new asteroid add total asteroids are : %d \n", len(newAsteroids))
+
+	// delete test
+	TargetID := newAsteroids[0].ID
+	DeleteAsteroid(TargetID)
+
+	var updatedAsteroids = GetAsteroids()
+	fmt.Printf("after the 1st asteroid deletion number of asteroids are : %d", len(updatedAsteroids))
 }
--- storage.go
+++ storage.go
@@ -8,5 +8,22 @@ var AsteroidDB []Asteroid
 
 func InitStorage(data []Asteroid) {
 	AsteroidDB = data
-	fmt.Printf("Success! the data has been extracted. \n")
+	fmt.Printf("Success! the data has been stored. \n")
+}
+
+func GetAsteroids() []Asteroid {
+	return AsteroidDB
+}
+
+func CreateAsteroid(newAsteroid Asteroid) {
+	AsteroidDB = append(AsteroidDB, newAsteroid)
+}
+
+func DeleteAsteroid(id string) {
+	for i := 0; i < len(AsteroidDB); i++ {
+		if AsteroidDB[i].ID == id {
+			AsteroidDB = append(AsteroidDB[:i], AsteroidDB[i+1:]...)
+			return
+		}
+	}
 }
