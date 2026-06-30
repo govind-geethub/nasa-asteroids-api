bb1a12c05cd65a37e27b99ffeb6e3cbbce80ed32
govind-geethub <avin14a@gmail.com>
2026-07-01 00:35:19 +0530

    successfully integrated the postgreSQL into my backend

--- main.go
+++ main.go
@@ -9,44 +9,31 @@ import (
 
 func main() {
 
+	// initialising postgres connection pool first
+	InitStorage()
+	defer DB.Close() // safely closes database connection when project stops
+
+	// fetch data from the NASA API
 	asteroids, err := fetchAsteroid()
 	if err != nil {
-		log.Fatalf("failed to fetch data")
+		log.Fatalf("failed to fetch the data \n")
 	}
 
-	if len(asteroids) == 0 {
-		fmt.Println("Warning: NASA returned 0 items. Injecting mock testing data...")
-		asteroids = []Asteroid{
-			{ID: "2001863", Name: "1863 Antinous (1948 EA)", AbsoluteMagnitudeH: 15.32, IsPotentiallyHazardousAsteroid: false},
-			{ID: "2001915", Name: "1915 Quetzalcoatl (1953 EA)", AbsoluteMagnitudeH: 18.38, IsPotentiallyHazardousAsteroid: false},
-			{ID: "2001917", Name: "1917 Cuyo (1968 AA)", AbsoluteMagnitudeH: 14.38, IsPotentiallyHazardousAsteroid: false},
-		}
+	// save the the data direct into the database
+	for _, asteroid := range asteroids {
+		CreateAsteroid(asteroid)
 	}
-
-	InitStorage(asteroids)
-	fmt.Printf("Fetched %d asteroids from NASA. \n", len(asteroids))
+	fmt.Printf("Processed %d asteroids into PostgreSQL database \n", len(asteroids))
 
 	var savedAsteroids = GetAsteroids(0, 100, false, false)
 	fmt.Printf("number of saved asteroids are : %d \n", len(savedAsteroids))
-
-	var newAsteroids = GetAsteroids(0, 100, false, false)
-	fmt.Printf("after new asteroid add total asteroids are : %d \n", len(newAsteroids))
-
-	// delete test
-	// TargetID := newAsteroids[0].ID
-	// DeleteAsteroid(TargetID)
-
-	// var updatedAsteroids = GetAsteroids(0, 100, false, false)
-	// fmt.Printf("after the 1st asteroid deletion number of asteroids are : %d \n", len(updatedAsteroids))
-
-	// http.HandleFunc("/asteroids", GetAsteroidHandler)
-	// http.HandleFunc("/asteroids/create", CreateAsteroidHandler)
 
 	// RESTful routing mappings
 	http.HandleFunc("/asteroids", GetAsteroidHandler)
 	http.HandleFunc("/asteroids/create", CreateAsteroidHandler)
 	http.HandleFunc("/asteroids/delete", DeleteAsteroidHandler)
 
+	// spin up the network engine
 	log.Println("Server starting on Port : 8080...")
 	http.ListenAndServe(":8080", nil)
 }
--- storage.go
+++ storage.go
@@ -1,66 +1,94 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
+
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
-var AsteroidDB []Asteroid
+// global database connection pool pointer
+var DB *pgxpool.Pool
 
-func InitStorage(data []Asteroid) {
-	AsteroidDB = data
-	fmt.Printf("Success! the data has been stored. \n")
+// InitStorage hooks the backend to postgres directly
+func InitStorage() {
+	connStr := "postgres://postgres:Avin14AG07@localhost:5432/nasa_space_project"
+
+	var err error
+	DB, err = pgxpool.New(context.Background(), connStr)
+	if err != nil {
+		log.Fatalf("Unable to connect to database: %v \n", err)
+	}
+
+	// verify the connections work
+	err = DB.Ping(context.Background())
+	if err != nil {
+		log.Fatalf("Database ping failed: %v \n", err)
+	}
+
+	fmt.Println("Success! Connected to the PostgreSQL.\n")
 }
 
+// inserts a new asteroid row into the database
+func CreateAsteroid(newAsteroid Asteroid) {
+	query := `
+			INSERT INTO asteroids (id, name, absolute_magnitude_h, is_potentially_hazardous_asteroid)
+			VALUES ($1, $2, $3, $4)
+			ON CONFLICT (id) DO NOTHING;`
+
+	_, err := DB.Exec(context.Background(), query,
+		newAsteroid.ID,
+		newAsteroid.Name,
+		newAsteroid.AbsoluteMagnitudeH,
+		newAsteroid.IsPotentiallyHazardousAsteroid,
+	)
+
+	if err != nil {
+		log.Printf("Failed to insert asteroid %s into the database: %v \n", newAsteroid.Name, err)
+	}
+}
+
+// GetAsteroids gives all the paginated data
 func GetAsteroids(offset int, limit int, filterHazardous bool, hazardousValue bool) []Asteroid {
+	var rows pgx.Rows
+	var err error
+
+	baseQuery := `SELECT id, name, absolute_magnitude_h, is_potentially_hazardous_asteroid FROM asteroids`
 
 	if filterHazardous {
-		sourceData := FilterAsteroids(hazardousValue)
+		sqlQuery := baseQuery + ` WHERE is_potentially_hazardous_asteroid = $1 LIMIT $2 OFFSET $3;`
+		rows, err = DB.Query(context.Background(), sqlQuery, hazardousValue, limit, offset)
+	} else {
+		sqlQuery := baseQuery + ` LIMIT $1 OFFSET $2;`
+		rows, err = DB.Query(context.Background(), sqlQuery, limit, offset)
+	}
 
-		// if offset has passed the last asteroid
-		if offset >= len(sourceData) {
-			return []Asteroid{} // empty slice
+	if err != nil {
+		log.Printf("Qe=uery failed: %v \n", err)
+	}
+	defer rows.Close()
+
+	var asteroids []Asteroid
+	for rows.Next() {
+		var a Asteroid
+		err := rows.Scan(&a.ID, &a.Name, &a.AbsoluteMagnitudeH, &a.IsPotentiallyHazardousAsteroid)
+		if err != nil {
+			log.Printf("Row scan failed: %v \n", err)
 		}
-
-		end := offset + limit
-		if end > len(sourceData) {
-			end = len(sourceData)
-		}
-
-		// return the slice window
-		return sourceData[offset:end]
+		asteroids = append(asteroids, a)
 	}
 
-	if offset >= len(AsteroidDB) {
-		return []Asteroid{}
-	}
-
-	end := offset + limit
-	if end > len(AsteroidDB) {
-		end = len(AsteroidDB)
-	}
-
-	return AsteroidDB[offset:end]
+	return asteroids
 }
 
-func CreateAsteroid(newAsteroid Asteroid) {
-	AsteroidDB = append(AsteroidDB, newAsteroid)
-}
-
+// DeleteAsteroid drops the row matching the requested ID
 func DeleteAsteroid(id string) {
-	for i := 0; i < len(AsteroidDB); i++ {
-		if AsteroidDB[i].ID == id {
-			AsteroidDB = append(AsteroidDB[:i], AsteroidDB[i+1:]...)
-			return
-		}
-	}
-}
+	query := `DELETE FROM asteroids WHERE id = $1;`
 
-func FilterAsteroids(hazardous bool) []Asteroid {
-	var filtered []Asteroid
-	for _, asteroid := range AsteroidDB {
-		if asteroid.IsPotentiallyHazardousAsteroid == hazardous {
-			filtered = append(filtered, asteroid)
-		}
+	_, err := DB.Exec(context.Background(), query, id)
+	if err != nil {
+		log.Printf("Failed to delete asteroid with ID %s : %v \n", id, err)
 	}
-	return filtered
 }
